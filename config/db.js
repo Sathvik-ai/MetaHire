8615fb7076b9a391e74bf8012ca04dc846335bf4
Sathvik Kiran.Koppolu <125252205+Sathvik-ai@users.noreply.github.com>
2025-07-07 09:31:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+  try {    
+await mongoose.connect(process.env.MONGO_URI as string);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
